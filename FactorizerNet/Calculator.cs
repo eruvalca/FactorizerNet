e928b5ec1f14c4c3de00445f6c454b06f70afde8
eruvalca <eruvalca@outlook.com>
2022-11-21 16:19:30 -0500

    Add project files.

---
+++ FactorizerNet/Calculator.cs
@@ -0,0 +1,28 @@
+﻿namespace FactorizerNet;
+
+public class Calculator
+{
+    /// <summary>
+    /// Given a number, print the factors per the specification
+    /// </summary>
+    public static void PrintFactors(int number)
+    {
+
+    }
+
+    /// <summary>
+    /// Given a number, print if it is perfect or not
+    /// </summary>
+    public static void IsPerfectNumber(int number)
+    {
+
+    }
+
+    /// <summary>
+    /// Given a number, print if it is prime or not
+    /// </summary>
+    public static void IsPrimeNumber(int number)
+    {
+
+    }
+}
---
+++ FactorizerNet/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using FactorizerNet;
+
+int number = GetNumberFromUser();
+
+Calculator.PrintFactors(number);
+Calculator.IsPerfectNumber(number);
+Calculator.IsPrimeNumber(number);
+
+Console.WriteLine("Press any key to quit...");
+Console.ReadKey();
+
+/// <summary>
+/// Prompt the user for an integer.  Make sure they enter a valid integer!
+/// 
+/// See the String Input lesson for TryParse() examples
+/// </summary>
+/// <returns>the user input as an integer</returns>
+static int GetNumberFromUser()
+{
+    throw new NotImplementedException();
+}
